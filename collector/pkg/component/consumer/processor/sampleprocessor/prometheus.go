8ae9d155b47d67a661d6c094ff66f4f3b6be407f
huxiangyuan <huxiangyuan@harmonycloud.cn>
2023-04-24 10:34:05 +0800

    Auto clean p9x for destoryed container after 1 day.
    Signed-off-by: huxiangyuan <huxiangyuan@harmonycloud.cn>

--- collector/pkg/component/consumer/processor/sampleprocessor/prometheus.go
+++ collector/pkg/component/consumer/processor/sampleprocessor/prometheus.go
@@ -3,6 +3,8 @@ package sampleprocessor
 import (
 	"context"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/Kindling-project/kindling/collector/pkg/component"
 	"github.com/Kindling-project/kindling/collector/pkg/model"
@@ -12,17 +14,18 @@ import (
 type prometheusP9xCache struct {
 	// Grpc API
 	client model.P9XServiceClient
-	// Pod - Url - P9x
-	p9xCache  map[string]map[string]float64
+	// <containreId, *prometheusContainerP9x>
+	p9xCache  sync.Map
 	hostName  string
+	timeout   int64
 	telemetry *component.TelemetryTools
 }
 
 func newPrometheusP9xCache(client model.P9XServiceClient, telemetry *component.TelemetryTools) *prometheusP9xCache {
 	return &prometheusP9xCache{
 		client:    client,
-		p9xCache:  make(map[string]map[string]float64, 0),
 		hostName:  getHostname(),
+		timeout:   24 * 3600, // 1 day
 		telemetry: telemetry,
 	}
 }
@@ -39,14 +42,11 @@ func getHostname() string {
 }
 
 func (cache *prometheusP9xCache) getP9x(trace *SampleTrace) float64 {
-	if len(cache.p9xCache) == 0 {
-		return 0
+	if p9xInterface, exist := cache.p9xCache.Load(trace.dataGroup.Labels.GetStringValue(constlabels.ContainerId)); exist {
+		p9x := p9xInterface.(*prometheusContainerP9x)
+		return p9x.p9xValues[trace.dataGroup.Labels.GetStringValue(constlabels.ContentKey)]
 	}
-	urlMap := cache.p9xCache[trace.dataGroup.Labels.GetStringValue(constlabels.ContainerId)]
-	if len(urlMap) == 0 {
-		return 0
-	}
-	return urlMap[trace.dataGroup.Labels.GetStringValue(constlabels.ContentKey)]
+	return 0
 }
 
 func (cache *prometheusP9xCache) updateP9xByGrpc() {
@@ -59,16 +59,45 @@ func (cache *prometheusP9xCache) updateP9xByGrpc() {
 		return
 	}
 
+	var p9x *prometheusContainerP9x
+	now := time.Now().Unix() // Set as second.
 	for _, data := range result.Datas {
-		cacheContainer := cache.p9xCache[data.ContainerId]
-		if len(cacheContainer) == 0 {
-			cacheContainer = make(map[string]float64)
-			cache.p9xCache[data.ContainerId] = cacheContainer
+		p9xInterface, exist := cache.p9xCache.Load(data.ContainerId)
+		if !exist {
+			p9x = newPrometheusContainerP9x(now)
+			cache.p9xCache.Store(data.ContainerId, p9x)
+		} else {
+			p9x = p9xInterface.(*prometheusContainerP9x)
 		}
-		// Update P9x.
-		if data.Value > 0 {
-			cacheContainer[data.Url] = data.Value
-			cache.telemetry.Logger.Infof("Update [P9x of %s]: %d", data.Url, data.Value)
+		p9x.updateP9x(data, now)
+	}
+
+	// Clean Expired ContainerId
+	cache.p9xCache.Range(func(k, v interface{}) bool {
+		p9x := v.(*prometheusContainerP9x)
+		if now-p9x.updateTime >= cache.timeout {
+			cache.telemetry.Logger.Infof("Clean Expired P9x for Container: %s", k)
+			cache.p9xCache.Delete(k)
 		}
+		return true
+	})
+}
+
+type prometheusContainerP9x struct {
+	updateTime int64
+	p9xValues  map[string]float64
+}
+
+func newPrometheusContainerP9x(now int64) *prometheusContainerP9x {
+	return &prometheusContainerP9x{
+		updateTime: now,
+		p9xValues:  make(map[string]float64, 0),
+	}
+}
+
+func (p9x *prometheusContainerP9x) updateP9x(data *model.P9XData, time int64) {
+	if data.Value > 0 {
+		p9x.p9xValues[data.Url] = data.Value
+		p9x.updateTime = time
 	}
 }
